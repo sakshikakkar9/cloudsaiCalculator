0c98d263fc31fd18d565047ef4b740f8b7b66f3d
sakshikakkar9 <sakahikakkar111@gmail.com>
2025-11-24 19:54:23 +0530

    Added index.html

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ function calculate() {
     const costPerStudent = parseFloat(document.getElementById("costPerStudent").value);
     const participants = parseFloat(document.getElementById("participants").value);
     const hours = parseFloat(document.getElementById("hours").value);
-    const costPerHour = parseFloat(document.getElementById("costPerHour").value);
 
-    if (isNaN(costPerStudent) || isNaN(participants) || isNaN(hours) || isNaN(costPerHour)) {
+    if (isNaN(costPerStudent) || isNaN(participants) || isNaN(hours)) {
         alert("Please fill all fields correctly!");
         return;
     }
 
-    // NEW UPDATED FORMULA
-    const result = costPerStudent - (participants * hours );
+    const result = costPerStudent - (participants * hours);
 
     const resultBox = document.getElementById("resultBox");
     resultBox.style.display = "block";
